8aa2d37990fe9b8aa88747d4364c8c40eaba9445
dante01yoon <bunggl@naver.com>
2021-10-02 16:09:57 +0900

    Update: first-touch

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+
+interface AppProps {
+
+}
+
+const App:NextPage<AppProps> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  
+}
+
+export default App;
