1350ee2bfaaba8e301a0c417e5fa51228410a964
dante01yoon <bunggl@naver.com>
2021-10-08 10:30:33 +0900

    Update: _app Provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { NextPage } from "next";
+import { appStore  } from "../src/stores";
+import { Provider} from "react-redux";
+
 interface AppProps {
 
 }
 
 const App:NextPage<AppProps> = () => {
   return (
-    <div>
+    <Provider store={appStore}>
+      <div>
 
-    </div>
+      </div>
+    </Provider>
   )
 }
 
